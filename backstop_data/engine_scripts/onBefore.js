b63c6ca3b3073d3b8297450bae3145aacab99405
Garris Shipon <garris@me.com>
2017-07-13 00:39:30 -0700

    add onBefore — read cookies from file

--- backstop_data/engine_scripts/onBefore.js
+++ backstop_data/engine_scripts/onBefore.js
@@ -1,16 +1,21 @@
-module.exports = function (casper, scenario, vp) {
-  // This script runs before your app loads. Edit here to log-in, load cookies or set other states required for your test.
-  console.log('onBefore.js has run for ' + vp.name + '.');
-};
+let cookiePath = "./backstop_data/cookies.json";
 
-// // EXAMPLE: LOGIN BEFORE RUNNING TESTS
-// module.exports = function(casper, scenario, vp) {
-//   casper.thenOpen(scenario.url, function(){
-//      if (this.exists('form#user-login-form')) {
-//        this.fill('form#loginForm',{
-//           'username': 'test',
-//           'password': 'changeme'
-//        }, true);
-//      }
-//   });
-// };
+const fs = require('fs');
+const path = require('path');
+cookiePath = path.resolve(cookiePath);
+
+let COOKIES = [];
+
+if (fs.existsSync(cookiePath)) {
+  COOKIES = COOKIES.concat(JSON.parse(fs.readFileSync(cookiePath)));
+} else {
+  console.log('Cookie file not found: ' + cookiePath);
+}
+
+module.exports = function (chromyChain, scenario, vp) {
+  console.log('onBefore.js is running for: ', vp.label);
+  COOKIES.forEach((cookie) => {
+     chromyChain.setCookie(cookie);
+  })
+  return chromyChain;
+};
