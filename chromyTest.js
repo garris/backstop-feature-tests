e5187058356124e98a97b8aabb895f8b6ae2f070
Garris Shipon <garris@me.com>
2017-07-12 23:55:19 -0700

    udpated tests

--- chromyTest.js
+++ chromyTest.js
@@ -4,19 +4,69 @@ const fs = require('fs');
 const onReady = require('./backstop_data/engine_scripts/onReady');
 
 
+const DOMAIN = 'https://garris.github.io/';
+const DOMAIN_PATH = DOMAIN + 'BackstopJS/examples/featureTests/';
+const COOKIES = [
+  {
+    "url": DOMAIN,
+    "expirationDate": 1528614000,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "test2",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "XYZ",
+    "id": 1000
+  },
+  {
+    "url": DOMAIN,
+    "expirationDate": 1528614000,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "test",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "ABCDE",
+    "id": 1001
+  },
+  {
+    "url": DOMAIN,
+    "expirationDate": 1528614000,
+    "hostOnly": false,
+    "httpOnly": false,
+    "name": "test3",
+    "sameSite": "no_restriction",
+    "secure": false,
+    "session": false,
+    "storeId": "0",
+    "value": "FOO",
+    "id": 1001
+  }
 
+]
 
-let chromy = new Chromy({chromeFlags: ['--window-size=320,480'], visible: true});
+let chromy = new Chromy({chromeFlags: ['--window-size=1280,800'], visible: true});
 
-chromy
-  .chain()
+var chromyChain = chromy.chain();
+
+chromyChain
   .console(function (text, consoleObj) {
     if (console[consoleObj.level]) {
       console[consoleObj.level]((consoleObj.level).toUpperCase() + ' > ', text);
     }
   })
-  .goto('examples/featureTests/index.html')
-  .wait(1)
+
+COOKIES.forEach((cookie) => {
+   chromyChain.setCookie(cookie);
+})
+
+chromyChain
+  .goto(DOMAIN_PATH)
+  .wait('._READY')
   .screenshotMultipleSelectors(
     ["body", "h1", "h2"],
     handlescreenshots,
@@ -34,6 +84,6 @@ chromy
 
 
 function handlescreenshots(error, png, index, selectors, sub){
-  console.log('>>>',error, selectors[index], sub);
+  console.log('saving >', selectors[index], sub);
   fs.writeFileSync(`./screens/${selectors[index]}_${sub}.png`, png);
 }
