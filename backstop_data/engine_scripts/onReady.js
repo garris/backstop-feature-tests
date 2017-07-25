3def9da776c035ccdb816b2a50e1fc9df7b642ab
Garris Shipon <garris@me.com>
2017-07-25 07:49:56 -0700

    updated refs

--- backstop_data/engine_scripts/onReady.js
+++ backstop_data/engine_scripts/onReady.js
@@ -1,13 +1,14 @@
 module.exports = function (chromy, scenario, vp) {
-  console.log('onReady.js is running for: ', vp.name);
+  console.log('>>>')
+  console.log(chromy.wait)
   return chromy
     .wait(scenario.someOnReadyScriptParameter)
+    .evaluate(`_vpLabel = '${vp.label}'`)
     .evaluate(function () {
-      console.log('Hi there from inside your browser.')
+      console.log('Testing viewport: ' + _vpLabel)
       document.querySelector('.jumbotron p').style.background = 'greenyellow';
       document.querySelector('.jumbotron p').style.color = '#333';
       document.querySelector('.jumbotron p').style.padding = '27px';
       return document.getElementsByTagName('body')[0].classList[0];
     })
-    .result(resultFromPreviousEval => console.log('result for ' + vp.label + ' >', resultFromPreviousEval));
 };
